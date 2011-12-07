d647785ac521ecc560f190434f43065d62cf81e6
bhartsock9287 <bhartsock9287@0411eb3a-b6a6-49f8-be28-3739617ebca6>
2011-12-07 14:43:45 +0000

    Update is now included in the openhds-core
    git-svn-id: http://svn.rcg.usm.maine.edu/svn/openhds/openhds-core@1846 0411eb3a-b6a6-49f8-be28-3739617ebca6

--- domain/src/main/java/org/openhds/domain/model/Gender.java
+++
@@ -1,5 +0,0 @@
-package org.openhds.domain.model;
-
-public enum Gender {
-	MALE, FEMALE, UNKNOWN
-}
--- domain/src/main/java/org/openhds/domain/model/Round.java
+++ domain/src/main/java/org/openhds/domain/model/Round.java
@@ -13,6 +13,7 @@ import org.openhds.domain.annotations.Description;
 import org.openhds.domain.constraint.CheckEndDateGreaterThanStartDate;
 import org.openhds.domain.constraint.CheckInteger;
 import org.openhds.domain.constraint.GenericStartEndDateConstraint;
+import org.openhds.domain.constraint.Searchable;
 
 @Description(description="A Round represents a range of dates in which Visits " +
 		"can take place.")
