9b05a5be0a62982a9926856cc47111ed11408e3a
droberge <droberge@0411eb3a-b6a6-49f8-be28-3739617ebca6>
2011-12-20 21:45:47 +0000

    Updating ConstraintViolations
    git-svn-id: http://svn.rcg.usm.maine.edu/svn/openhds/openhds-core@1878 0411eb3a-b6a6-49f8-be28-3739617ebca6

--- controller/src/main/java/org/openhds/controller/exception/ConstraintViolations.java
+++ controller/src/main/java/org/openhds/controller/exception/ConstraintViolations.java
@@ -1,11 +1,14 @@
 package org.openhds.controller.exception;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConstraintViolations extends Exception {
 
 	private static final long serialVersionUID = 4392790814928552607L;
-	private List<String> violations;
+	private List<String> violations = new ArrayList<String>();
+	
+	public ConstraintViolations() {}
 	
 	public ConstraintViolations(String msg, List<String> violations) {
 		super(msg);
@@ -14,9 +17,19 @@ public class ConstraintViolations extends Exception {
 	
 	public ConstraintViolations(String msg) {
 		super(msg);
+		violations.add(msg);
 	}
-
+	
 	public List<String> getViolations() {
 		return violations;
 	}
+
+	public void addViolations(String invalidFieldWorkerId) {
+		violations.add(invalidFieldWorkerId);
+	}
+
+	public boolean hasViolations() {
+		return violations != null && violations.size() > 0;
+	}
 }
+
