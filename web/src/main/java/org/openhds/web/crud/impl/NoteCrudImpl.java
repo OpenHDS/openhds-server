4201b3cfe9d54fb2a346ed545f4b87254186d979
bhartsock9287 <bhartsock9287@0411eb3a-b6a6-49f8-be28-3739617ebca6>
2012-04-24 16:37:33 +0000

    Adding missing classes, weren't added to previous commit.
    git-svn-id: http://svn.rcg.usm.maine.edu/svn/openhds/openhds-core@2073 0411eb3a-b6a6-49f8-be28-3739617ebca6

---
+++ web/src/main/java/org/openhds/web/crud/impl/NoteCrudImpl.java
@@ -0,0 +1,28 @@
+package org.openhds.web.crud.impl;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import org.openhds.domain.model.Note;
+
+public class NoteCrudImpl extends EntityCrudImpl<Note, String> {
+	
+    // used for manual conversion between Date and Calendar since the openFaces Calendar doesn't support JSF Converters
+    Date observationDate;
+	
+    public NoteCrudImpl(Class<Note> entityClass) {
+        super(entityClass);
+    }
+
+    public Date getObservationDate() {
+    	if (entityItem.getObservationDate() == null)
+    		return new Date();
+    	return entityItem.getObservationDate().getTime();
+	}
+
+	public void setObservationDate(Date observationDate) throws ParseException {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(observationDate);
+		entityItem.setObservationDate(cal);
+	}
+}
