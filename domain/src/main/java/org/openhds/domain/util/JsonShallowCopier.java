686f3882cfbc504d9e42f83e9b6e221738ca2364
littlefieldnick <nickolas.littlefield@maine.edu>
2018-11-05 01:23:35 -0500

    Updated JSONShallowCopy to return collectedBy for SocialGroups.

--- domain/src/main/java/org/openhds/domain/util/JsonShallowCopier.java
+++ domain/src/main/java/org/openhds/domain/util/JsonShallowCopier.java
@@ -12,100 +12,105 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JsonShallowCopier {
-	 private static final Logger logger = LoggerFactory.getLogger(JsonShallowCopier.class);
-	 
-	 public static Location copyLocation(Location loc) {
-	        Location copy = new Location();
+	private static final Logger logger = LoggerFactory.getLogger(JsonShallowCopier.class);
 
-	        copy.setAccuracy(getEmptyStringIfBlank(loc.getAccuracy()));
-	        copy.setAltitude(getEmptyStringIfBlank(loc.getAltitude()));
-	        copy.setLatitude(getEmptyStringIfBlank(loc.getLatitude()));
-	        copy.setLongitude(getEmptyStringIfBlank(loc.getLongitude()));
+	public static Location copyLocation(Location loc) {
+		Location copy = new Location();
 
-	        LocationHierarchy level = new LocationHierarchy();
-	        level.setExtId(loc.getLocationLevel().getExtId());
-	        level.setLevel(loc.getLocationLevel().getLevel());
-	        level.setName(loc.getLocationLevel().getName());
-	        level.setExtId(loc.getLocationLevel().getExtId());
-	        level.setParent(loc.getLocationLevel().getParent());
-	        copy.setLocationLevel(level);
-	        copy.setUuid(loc.getUuid());
-	        copy.setExtId(loc.getExtId());
-	        copy.setLocationName(loc.getLocationName());
-	        copy.setLocationType(loc.getLocationType());
-	        	copy.setInsertDate(loc.getInsertDate());
-	        	copy.setServerInsertTime(loc.getServerInsertTime());
-	        	copy.setServerUpdateTime(loc.getServerUpdateTime());
-	        FieldWorker fw = new FieldWorker();
-	        fw.setExtId(loc.getCollectedBy().getExtId());
-	        copy.setCollectedBy(fw);
-	        return copy;
-	    }
-	 
-	 public static Individual shallowCopyIndividual(Individual individual) {
+		copy.setAccuracy(getEmptyStringIfBlank(loc.getAccuracy()));
+		copy.setAltitude(getEmptyStringIfBlank(loc.getAltitude()));
+		copy.setLatitude(getEmptyStringIfBlank(loc.getLatitude()));
+		copy.setLongitude(getEmptyStringIfBlank(loc.getLongitude()));
 
-	        Individual copy = new Individual();
-	        try {
-	        		copy.setCollectedBy(individual.getCollectedBy());
-	            copy.setDob(individual.getDob());
-	            copy.setExtId(individual.getExtId());
-	            copy.setUuid(individual.getUuid());
-	            copy.setFirstName(individual.getFirstName());
-	            copy.setGender(individual.getGender());
-	            copy.setLastName(individual.getLastName());
-	            String middleName = individual.getMiddleName() == null ? "" : individual.getMiddleName();
-	            copy.setMiddleName(middleName);
-	            String religion = individual.getReligion() == null ? "" : individual.getReligion();
-	            copy.setReligion(religion);
-	            Individual father = shallowCopyIndividual(individual.getFather());
-	            copy.setMother(father);
-	            Individual mother = shallowCopyIndividual(individual.getMother());
-	            copy.setMother(mother);
-	            
+		LocationHierarchy level = new LocationHierarchy();
+		level.setExtId(loc.getLocationLevel().getExtId());
+		level.setLevel(loc.getLocationLevel().getLevel());
+		level.setName(loc.getLocationLevel().getName());
+		level.setExtId(loc.getLocationLevel().getExtId());
+		level.setParent(loc.getLocationLevel().getParent());
+		copy.setLocationLevel(level);
+		copy.setUuid(loc.getUuid());
+		copy.setExtId(loc.getExtId());
+		copy.setLocationName(loc.getLocationName());
+		copy.setLocationType(loc.getLocationType());
+		copy.setInsertDate(loc.getInsertDate());
+		copy.setServerInsertTime(loc.getServerInsertTime());
+		copy.setServerUpdateTime(loc.getServerUpdateTime());
+		FieldWorker fw = new FieldWorker();
+		fw.setExtId(loc.getCollectedBy().getExtId());
+		copy.setCollectedBy(fw);
+		return copy;
+	}
 
-	            
-	            if (individual.getCurrentMembership() != null /*&& individual.getCurrentMembership().getEndDate()==null*/) {
-	            	Membership memCopy = new Membership();
-	                SocialGroup sgCopy = new SocialGroup();
-	                sgCopy.setExtId(individual.getCurrentMembership().getSocialGroup().getExtId());
+	public static Individual shallowCopyIndividual(Individual individual) {
 
-	                memCopy.setSocialGroup(sgCopy);
-	                memCopy.setbIsToA(individual.getCurrentMembership().getbIsToA());
-	                copy.getAllMemberships().add(memCopy);
-	            }
+		Individual copy = new Individual();
+		try {
+			FieldWorker fw = new FieldWorker();
+			fw.setExtId(individual.getCollectedBy().getExtId());
+			copy.setCollectedBy(fw);
+			copy.setDob(individual.getDob());
+			copy.setExtId(individual.getExtId());
+			copy.setUuid(individual.getUuid());
+			copy.setFirstName(individual.getFirstName());
+			copy.setGender(individual.getGender());
+			copy.setLastName(individual.getLastName());
+			String middleName = individual.getMiddleName() == null ? "" : individual.getMiddleName();
+			copy.setMiddleName(middleName);
+			String religion = individual.getReligion() == null ? "" : individual.getReligion();
+			copy.setReligion(religion);
+			Individual father = shallowCopyIndividual(individual.getFather());
+			copy.setMother(father);
+			Individual mother = shallowCopyIndividual(individual.getMother());
+			copy.setMother(mother);
 
-	            if (individual.getCurrentResidency() != null) {
-	                Residency resCopy = new Residency();
-	                resCopy.setEndType(individual.getCurrentResidency().getEndType());
-	                Location locCopy = new Location();
-	                locCopy.setLocationLevel(null);
-	                locCopy.setExtId(individual.getCurrentResidency().getLocation().getExtId());
-	                resCopy.setLocation(locCopy);
-	                copy.getAllResidencies().add(resCopy);
-	            }
-	        } catch (Exception e) {
-	            System.out.println(copy.getExtId());
-	        }
-	        return copy;
-	    }
-	 
-	 public static SocialGroup copySocialGroup(SocialGroup original) {
-	        SocialGroup copy = new SocialGroup();
-	        copy.setExtId(original.getExtId());
-	        copy.setUuid(original.getUuid());
-	        Individual groupHead = shallowCopyIndividual(original.getGroupHead());
-	        copy.setGroupHead(groupHead);
-	        copy.setGroupName(original.getGroupName());
-	        return copy;
-	    }
-	 
-	  private static String getEmptyStringIfBlank(String accuracy) {
-	        if (accuracy == null || accuracy.trim().isEmpty()) {
-	            return "";
-	        }
 
-	        return accuracy;
-	    }
+
+			if (individual.getCurrentMembership() != null /*&& individual.getCurrentMembership().getEndDate()==null*/) {
+				Membership memCopy = new Membership();
+				SocialGroup sgCopy = new SocialGroup();
+				sgCopy.setExtId(individual.getCurrentMembership().getSocialGroup().getExtId());
+
+				memCopy.setSocialGroup(sgCopy);
+				memCopy.setbIsToA(individual.getCurrentMembership().getbIsToA());
+				copy.getAllMemberships().add(memCopy);
+			}
+
+			if (individual.getCurrentResidency() != null) {
+				Residency resCopy = new Residency();
+				resCopy.setEndType(individual.getCurrentResidency().getEndType());
+				Location locCopy = new Location();
+				locCopy.setLocationLevel(null);
+				locCopy.setExtId(individual.getCurrentResidency().getLocation().getExtId());
+				resCopy.setLocation(locCopy);
+				copy.getAllResidencies().add(resCopy);
+			}
+		} catch (Exception e) {
+			System.out.println(copy.getExtId());
+		}
+		return copy;
+	}
+
+	public static SocialGroup copySocialGroup(SocialGroup original) {
+		SocialGroup copy = new SocialGroup();
+		FieldWorker fw = new FieldWorker();
+		fw.setExtId(original.getCollectedBy().getExtId());
+		copy.setCollectedBy(fw);
+		copy.setExtId(original.getExtId());
+		copy.setUuid(original.getUuid());
+		Individual groupHead = shallowCopyIndividual(original.getGroupHead());
+		copy.setGroupHead(groupHead);
+		copy.setGroupName(original.getGroupName());
+		return copy;
+	}
+
+	private static String getEmptyStringIfBlank(String accuracy) {
+		if (accuracy == null || accuracy.trim().isEmpty()) {
+			return "";
+		}
+
+		return accuracy;
+	}
 
 	public static FieldWorker shallowCopyFieldWorker(FieldWorker fieldWorker) {
 		FieldWorker copy = new FieldWorker();
@@ -116,7 +121,7 @@ public class JsonShallowCopier {
 		copy.setPasswordHash(fieldWorker.getPasswordHash());
 		return copy;
 	}
-	
+
 	public static User shallowCopyUser(User user) {
 		User copy = new User();
 		copy.setUuid(user.getUuid());
